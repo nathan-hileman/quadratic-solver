4c03594c1f3268e52c5fd759bbed09a8c09c503c
nathan-hileman <nathan.r.hileman@wmich.edu>
2018-02-18 17:56:29 -0500

    q_solve passes root_1, root_2 by reference

--- src/q_solve/q_solve.c
+++ src/q_solve/q_solve.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
 #include <math.h>
 #include "q_solve.h"
-#include "log_output.h"
 
 /* Function that implements the quadratic equation.
  * Takes floats a, b, and c from user input and uses them in the equation.
  */
-int q_solve(float a, float b, float c) {   
+int q_solve(float a, float b, float c, float *root_1, float *root_2)
+{   
     
     /* Declaration of Variables */
-    float d, root_1, root_2;  // discrimanant, and the two possible root solutions
+    float disc  // discriminant
  
     /* Equation for the Discriminant */
-    d = (b * b) - (4 * a * c);
+    disc = (b * b) - (4 * a * c);
 
     /* Discriminant Positive, Real Number Solution */
-    if (d > 0) {
-
+    if (disc > 0) {
+        
         printf("Roots are real numbers.\n");
   
         /* Calculate Solution of Both Roots */
-        root_1 = (-b + sqrt(d)) / (2 * a);
-        root_2 = (-b - sqrt(d)) / (2 * a);
+        *root_1 = (-b + sqrt(d)) / (2 * a);
+        *root_2 = (-b - sqrt(d)) / (2 * a);
 
-        printf("Roots of quadratic equation are: %.7f , %.7f", root_1, root_2);
-    
-    } else if (d == 0) { /* Discriminant Zero, One Real Double Root Solution */
+        printf("Roots of quadratic equation are: %.7f , %.7f", (void *) root_1, (void *) root_2);
 
+    } else if (disc == 0) {  /* Discriminant Zero, One Real Double Root Solution */
+        
         printf("Double root solution.\n");
  
         /* Calculate Single Double Root Solution */
-        root_1 = -b / (2 * a);
+        *root_1 = -b / (2 * a);
+        *root_2 = b / (2 * a);
 
-        printf("Root of quadratic equation is +/-: %.7f ", root_1);
+        printf("Root of quadratic equation is +/-: %.7f ", (void *) root_1);
  
         return 0;
-    } else { /* Discriminant Negative, Complex Number Solution (IGNORE) */
-      
+        
+    } else {    /* Discriminant Negative, Complex Number Solution (IGNORE) */
+        
         printf("Complex roots. No real solution.\n");
     
         return 0; 
--- src/quad_solver.c
+++ src/quad_solver.c
@@ -4,6 +4,7 @@
 #include "./read_line/read_line.h"
 #include "./log_output/log_output.h"
 #include "./validate/validate_line.h"
+#include "./q_solve/q_solve.h"
 
 /**
 * Controller for the quadratic equation solver
@@ -12,9 +13,9 @@ int main()
 {
     /* Declare and Initialize Variables */
     int n = 100;
-    double a = 0.0;
-    double b = 0.0;
-    double c = 0.0;
+    float a = 0.0, b = 0.0, c = 0.0;                            // input variables for quadratic equation
+    float root_1 = 0.0, root_2 = 0.0;                           // root solution(s) to quadratic equation
+    char *buffer = malloc(sizeof(char) * 100);                  // output buffer
 
     log_output("Command Line Quadratic Solver\n");
 
@@ -39,13 +40,15 @@ int main()
     /* Ingest Into Quadratic Equation and Solve */
     log_output("q_solve:    ")
 
-    if ( q_solve(a, b, c) != 0 )                                // if q_solve received an error
+    if ( q_solve(a, b, c, &root1, &root2) != 0 )                // if q_solve received an error
     {
         log_output("q_solve failed");                               // report failure to log file
     }
     else                                                        // if q_solve returned successfuly
     {
         log_output("q_solve successful");                           // report success to log file
+        sprintf(buffer, "root1: %.7f, root2: %.7f");                // write solutions to an output buffer 
+        log_output(buffer);                                         // write output buffer to log file
     }
 
 }
